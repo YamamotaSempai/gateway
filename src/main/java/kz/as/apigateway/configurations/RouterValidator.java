90dbcf14b8436b663329fdceb042aaf7575cb92f
YamamotaSempai <myLittlePoni>
2021-08-06 17:52:12 +0600

    feat(jwt): add routes for login/register

---
+++ src/main/java/kz/as/apigateway/configurations/RouterValidator.java
@@ -0,0 +1,24 @@
+package kz.as.apigateway.configurations;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import java.util.List;
+
+
+public class RouterValidator {
+
+    private RouterValidator() {
+    }
+
+    private static final List<String> OPEN_API_ENDPOINTS = List.of(
+            "/auth/register",
+            "/auth/login"
+    );
+
+    public static boolean isSecured(ServerHttpRequest request) {
+        return OPEN_API_ENDPOINTS
+                .stream()
+                .anyMatch(uri -> request.getURI().getPath().contains(uri));
+    }
+
+}
--- src/main/java/kz/as/apigateway/configurations/RoutingConfig.java
+++ src/main/java/kz/as/apigateway/configurations/RoutingConfig.java
@@ -1,5 +1,6 @@
-package kz.as.apigateway.configuration;
+package kz.as.apigateway.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RoutingConfig {
 
+    private final AuthenticationFilter authenticationFilter;
+
+    @Autowired
+    public RoutingConfig(AuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-route", r -> r
                         .path("/auth/**")
-                        .filters(f -> f.stripPrefix(1))
+                        .filters(f -> f.filter(authenticationFilter))
                         .uri("lb://auth-service/")
                 )
                 .build();
